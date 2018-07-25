f53bc4cfa6e27a94a06dfb994de59bafa8143b52
zhengliang <1329901494@qq.com>
2018-07-25 20:47:28 +0800

    配置完成根目录,首页,购物车页

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--顶部-->
+    <!-- 顶部通栏 -->
+    <div class="header">
+      <!-- 1.0 导航栏头部 -->
+      <div class="head-top">
+        <div class="section">
+          <div class="left-box">
+            <span>黑马买买买</span>
+            <a target="_blank" href="#"></a>
+            <a target="_blank" href="#"></a>
+          </div>
+          <div id="menu" class="right-box">
+            <span style="display: none;">
+              <a href="" class="">登录</a>
+              <strong>|</strong>
+              <a href="" class="">注册</a>
+              <strong>|</strong>
+            </span>
+            <span>
+              <router-link to="/member">会员中心</router-link>
+              <strong>|</strong>
+              <a>退出</a>
+              <strong>|</strong>
+            </span>
+            <router-link to="/car">
+              <i class="iconfont icon-cart"></i>购物车(
+              <span id="shoppingCartCount">
+                <span>4</span>
+              </span>)</router-link>
+          </div>
+        </div>
+      </div>
+
+      <!-- 2.0 导航条 -->
+      <div class="head-nav">
+        <div class="section">
+          <div id="menu2" class="nav-box menuhd">
+            <ul>
+              <li class="index">
+                <router-link to="/index">
+                  <span class="out" style="top: 0px;">首页</span>
+                </router-link>
+              </li>
+              <li class="news">
+                <router-link to="/">
+                  <span class="out" style="top: 0px;">每日精选</span>
+                </router-link>
+              </li>
+              <li class="photo">
+                <a href="#" class="">
+                  <span class="out" style="top: 0px;">秒杀专区</span>
+                </a>
+              </li>
+              <li class="video">
+                <a href="#" class="">
+                  <span class="out" style="top: 0px;">黑马超市</span>
+                </a>
+              </li>
+              <li class="down">
+                <a href="#" class="">
+                  <span class="out" style="top: 0px;">会员权益</span>
+                </a>
+              </li>
+              <li class="goods">
+                <a href="" class="router-link-exact-active ">
+                  <span class="out" style="top: 0px;">购物商城</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="search-box">
+            <div class="input-box">
+              <input id="keywords" name="keywords" type="text" onkeydown="if(event.keyCode==13){};" placeholder="输入关健字" x-webkit-speech="">
+            </div>
+            <a href="javascript:;">
+              <i class="iconfont icon-search"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 路由的出口 -->
+    <router-view></router-view>
+    <!--底部-->
+    <div data-v-7ba5bd90="" class="footer">
+      <div data-v-7ba5bd90="" class="section">
+        <div data-v-7ba5bd90="" class="foot-nav">
+          <a data-v-7ba5bd90="" href="">关于我们</a>
+          <strong data-v-7ba5bd90="">|</strong>
+          <a data-v-7ba5bd90="" href="">联系我们</a>
+          <strong data-v-7ba5bd90="">|</strong>
+          <a data-v-7ba5bd90="" href="">联系客服</a>
+          <strong data-v-7ba5bd90="">|</strong>
+          <a data-v-7ba5bd90="" href="">合作招商</a>
+          <strong data-v-7ba5bd90="">|</strong>
+          <a data-v-7ba5bd90="" href="">商家帮助</a>
+          <strong data-v-7ba5bd90="">|</strong>
+          <a data-v-7ba5bd90="" href="">营销中心</a>
+          <strong data-v-7ba5bd90="">|</strong>
+          <a data-v-7ba5bd90="" href="">隐私政策</a>
+        </div>
+        <div data-v-7ba5bd90="" class="foot-box">
+          <div data-v-7ba5bd90="" class="copyright">
+            <p data-v-7ba5bd90="">版权所有 黑马买买买 </p>
+            <p data-v-7ba5bd90="">公司地址： 联系电话：</p>
+            <p data-v-7ba5bd90="" class="gray">Copyright © 2009-2018 itcast Corporation,All Rights Reserved.</p>
+          </div>
+          <div data-v-7ba5bd90="" class="service">
+            <p data-v-7ba5bd90="">周一至周日 9:00-24:00</p>
+            <a data-v-7ba5bd90="" href="#">
+              <i data-v-7ba5bd90="" class="iconfont icon-phone"></i>在线客服</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  /* @import url('./assets/statics/site/css/style.css') */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由模块
+import VueRouter from 'vue-router'
+// 引入index组件(首页)
+import index from './components/index.vue'
+// 引入member组件(会员中兴页)
+import member from './components/member.vue'
+// 引入car组件(购物车页)
+import car from './components/car.vue'
+// 使用路由中间件
+Vue.use(VueRouter);
+
+// 注册路由规则
+const router = new VueRouter({
+  routes:[
+    {
+      path:'/index',
+      component:index
+    },{
+      path:'/member',
+      component:member
+    },{
+      path:'/car',
+      component:car
+    }
+  ]
+})
+
+// 引入css
+import './assets/statics/site/css/style.css';
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 选择器
+  el:"#app",
+  // 挂载到vue
+  router,
+  // 渲染 App组件
   render: h => h(App)
-}).$mount('#app')
+})
